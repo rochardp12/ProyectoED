de37527394783b690f518189878dd6974eb3da49
unknown <ricluper@espol.edu.ec>
2022-06-26 04:15:47 -0500

    Pantalla iniciar sesion

---
+++ src/main/java/ec/edu/espol/controllers/PantallaIniciarSesionController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author Richard
+ */
+public class PantallaIniciarSesionController implements Initializable {
+
+    @FXML
+    private TextField infoUsuario;
+    @FXML
+    private Button btnIniciar;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void iniciarSesion(MouseEvent event) {
+    }
+    
+}
